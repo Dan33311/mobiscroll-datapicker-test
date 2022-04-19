4319c76ef069473cec371bcb8d34d464c80ffaab
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2022-04-18 22:53:48 -0500

    mobiscroll datapicker working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+// import { useState } from "react";
+import { Datepicker } from "@mobiscroll/react";
+import "@mobiscroll/react/dist/css/mobiscroll.min.css";
+
 import './App.css'
 
 function App() {
+  // const [birthday, setBirthday] = useState(null);
+  //   const onBirthdayChange = (ev) => {
+  //       setBirthday(ev.value);
+  //   }
 
   return (
     <div className="App">
       <h1>mobiscroll datapicker test</h1>
+      <Datepicker 
+        controls={['calendar', 'time']}
+        selectMultiple={false}
+        min="2022-04-18"
+        max="2050-01-01"
+        theme="ios"
+        themeVariant="dark"
+        // label="Travel date"
+        touchUi={true}
+        inputComponent="input"
+        inputProps={{ 
+          placeholder: 'Travel date...' 
+        }} 
+        // display="inline"
+      />
     </div>
   )
 }
